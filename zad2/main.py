80e293301e6fc6c857dcf1b80f302fb5bcb051ef
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-04-18 08:21:02 +0200

    Update main.py

--- zad2/main.py
+++ zad2/main.py
@@ -2,6 +2,15 @@ import function
 import math
 from bat.bat_algorithm import run_algorithm
 
+
+# Najlepiej dzialalo na parametrach:
+# alpha: 0.9
+# gamma: 0.9
+# f_min: 0
+# f_max: 2
+# pop_size: 1000
+# iterations: 1000
+
 best_pos, best_fit = run_algorithm(20, 1000, 1000, function.Levy(), 0, 2, 0.9, 0.9)
 print(round(best_fit, 4))
 print(best_pos)
