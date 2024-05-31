2e725d920c12d65e588641466c11efb37496a254
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-05-31 13:44:18 +0200

    Rename

--- zad3/main.py
+++ zad3/main.py
@@ -1,5 +1,5 @@
 import function
-import zad3.genetic_learning_pso.pso_alg as pso_alg
+import zad3.pso_with_pattern.pso_alg as pso_alg
 
 best_pos, best_fit = pso_alg.run_pso(function.Rastrigin(), 1000, 500, 0.5, 0.9, 0.9, 0.5)
 
