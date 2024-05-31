7d19f2c9f0e5bdcc930cc106aa9470cde02b3711
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-05-31 15:40:43 +0200

    SMA multiswarm

--- zad3/main.py
+++ zad3/main.py
@@ -2,8 +2,8 @@ import function
 import zad3.pso_with_pattern.pso_alg as pso_alg
 import zad3.sma_with_pattern.sma_alg as sma_alg
 
-best_pos, best_fit = pso_alg.run_pso(function.Schwefel(), 100, 10000, 0.5, 0.9, 0.9, 0.5, 10)
+best_pos, best_fit = pso_alg.run_pso(function.Sphere(), 1000, 500, 0.5, 0.9, 0.9, 0.5, 2)
 print(round(best_fit, 4))
 
-#best_pos_sma, best_fit_sma = sma_alg.run_alg(function.Schwefel(), 100, 100, 0.9, 0.0)
-#print(round(best_fit_sma, 4))
+best_pos_sma, best_fit_sma = sma_alg.run_alg(function.Sphere(), 1000, 500, 0.9, 0.0, 2)
+print(round(best_fit_sma, 4))
--- zad3/sma_with_pattern/sma_alg.py
+++ zad3/sma_with_pattern/sma_alg.py
@@ -5,41 +5,60 @@ import random
 import numpy as np
 
 DIM_NUMBER = 20
-def run_alg(function: (), iterations: int, pop_size: int, w: float, vb: float):
-    positions = [
+def run_alg(function: (), iterations: int, pop_size: int, w: float, vb: float, number_of_swarms=1):
+    global_positions = [
         [random.uniform(function.x_min, function.x_max) for _ in range(DIM_NUMBER)] for _ in range(pop_size)
     ]
 
+    individuals_per_swarm = int(pop_size / number_of_swarms)
+    swarm_positions = []
+    last_individual_index = 0
+    for i in range(number_of_swarms):
+        swarm_positions.append(global_positions[last_individual_index:(last_individual_index + individuals_per_swarm)])
+        last_individual_index += individuals_per_swarm
+
     for i in range(iterations):
-        fitness = [function(position) for position in positions]
-        positions = [position for _, position in sorted(zip(fitness, positions))]
+        for swarm_no in range(number_of_swarms):
+            positions = swarm_positions[swarm_no]
+            fitness = [function(position) for position in positions]
+            positions = [position for _, position in sorted(zip(fitness, positions))]
 
-        w = w * math.exp(-i / iterations)
+            w = w * math.exp(-i / iterations)
 
-        # update positions
-
-        for j in range(pop_size):
-            if random.uniform(0, 1) < w:
-                # approach food
-                best_position = positions[0]
-                r = random.uniform(0, 1)
+            # update positions
+            swarm_population = len(positions)
+            for j in range(swarm_population):
+                if random.uniform(0, 1) < w:
+                    # approach food
+                    best_position = positions[0]
+                    r = random.uniform(0, 1)
+                    for k in range(DIM_NUMBER):
+                        positions[j][k] = positions[j][k] + r * (best_position[k] - positions[j][k])
+                else:
+                    # move randomly
+                    random_position = positions[random.randint(0, swarm_population - 1)]
+                    r = random.uniform(0, 1)
+                    for k in range(DIM_NUMBER):
+                        positions[j][k] = positions[j][k] + r * (random_position[k] - positions[j][k])
+                # vibrate
                 for k in range(DIM_NUMBER):
-                    positions[j][k] = positions[j][k] + r * (best_position[k] - positions[j][k])
-            else:
-                # move randomly
-                random_position = positions[random.randint(0, pop_size - 1)]
-                r = random.uniform(0, 1)
+                    positions[j][k] = positions[j][k] + random.uniform(-vb, vb)
+
+                # check boundaries
                 for k in range(DIM_NUMBER):
-                    positions[j][k] = positions[j][k] + r * (random_position[k] - positions[j][k])
-            # vibrate
-            for k in range(DIM_NUMBER):
-                positions[j][k] = positions[j][k] + random.uniform(-vb, vb)
+                    if positions[j][k] < function.x_min:
+                        positions[j][k] = function.x_min
+                    elif positions[j][k] > function.x_max:
+                        positions[j][k] = function.x_max
 
-            # check boundaries
-            for k in range(DIM_NUMBER):
-                if positions[j][k] < function.x_min:
-                    positions[j][k] = function.x_min
-                elif positions[j][k] > function.x_max:
-                    positions[j][k] = function.x_max
-
-    return positions[0], function(positions[0])
+        global_best_fitness = float('inf')
+        global_best_position = []
+        for swarm_no in range(number_of_swarms):
+            current_swarm_positions = swarm_positions[swarm_no]
+            current_swarm_fitness = [function(position) for position in current_swarm_positions]
+            best_swarm_fitness = min(current_swarm_fitness)
+            best_swarm_position = current_swarm_positions[current_swarm_fitness.index(best_swarm_fitness)]
+            if best_swarm_fitness < global_best_fitness:
+                global_best_fitness = best_swarm_fitness
+                global_best_position = best_swarm_position
+        return global_best_position, global_best_fitness
